722b2871268a90f510d3251fd21df7a99ab8988f
Yasuhiko Nara <narayasuhiko3@gmail.com>
2020-07-03 13:36:32 +0900

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import { Provider } from "react-redux";
 import { useStore } from "../store";
 import "mapbox-gl/dist/mapbox-gl.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css";
 
 export default function App({ Component, pageProps }) {
   const store = useStore(pageProps.initialReduxState);
