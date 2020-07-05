fdf5b10268796360ca699274c21839bde4aeaa38
ryo <ryoutakaya3623@Yahoo.co.jp>
2020-07-05 11:18:48 +0900

    add reset.css to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { Provider } from "react-redux";
 import { createStore } from "../redux/store/store";
 import "../styles/style.css"; //global css
+import "../styles/reset.css";
 
 export default function App({ Component, pageProps }) {
   const store = createStore(pageProps.initialReduxState);
