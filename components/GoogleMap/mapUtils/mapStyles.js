7c0ee9d689061324adce2c64393ba65ac6b9a3c6
Yasuhiko Nara <narayasuhiko3@gmail.com>
2020-07-06 22:24:38 +0900

    changed map style

--- components/GoogleMap/mapUtils/mapStyles.js
+++ components/GoogleMap/mapUtils/mapStyles.js
@@ -1,163 +1,52 @@
 export default [
   {
-    elementType: "labels",
-    stylers: [
-      {
-        visibility: "off",
-      },
-      {
-        color: "#f49f53",
-      },
-    ],
-  },
-  {
     featureType: "landscape",
+    elementType: "all",
     stylers: [
-      {
-        color: "#f9ddc5",
-      },
-      {
-        lightness: -7,
-      },
-    ],
-  },
-  {
-    featureType: "road",
-    stylers: [
-      {
-        color: "#813033",
-      },
-      {
-        lightness: 43,
-      },
-    ],
-  },
-  {
-    featureType: "poi.business",
-    stylers: [
-      {
-        color: "#645c20",
-      },
-      {
-        lightness: 38,
-      },
-    ],
-  },
-  {
-    featureType: "water",
-    stylers: [
-      {
-        color: "#1994bf",
-      },
-      {
-        saturation: -69,
-      },
-      {
-        gamma: 0.99,
-      },
-      {
-        lightness: 43,
-      },
-    ],
-  },
-  {
-    featureType: "road.local",
-    elementType: "geometry.fill",
-    stylers: [
-      {
-        color: "#f19f53",
-      },
-      {
-        weight: 1.3,
-      },
       {
         visibility: "on",
       },
+    ],
+  },
+  {
+    featureType: "poi.business",
+    elementType: "all",
+    stylers: [
       {
-        lightness: 16,
+        visibility: "simplified",
       },
     ],
   },
   {
     featureType: "poi.business",
+    elementType: "labels",
+    stylers: [
+      {
+        visibility: "simplified",
+      },
+    ],
   },
   {
     featureType: "poi.park",
+    elementType: "all",
     stylers: [
       {
-        color: "#645c20",
-      },
-      {
-        lightness: 39,
+        visibility: "off",
       },
     ],
   },
   {
     featureType: "poi.school",
+    elementType: "all",
     stylers: [
       {
-        color: "#a95521",
-      },
-      {
-        lightness: 35,
+        visibility: "on",
       },
     ],
-  },
-  {},
-  {
-    featureType: "poi.medical",
-    elementType: "geometry.fill",
-    stylers: [
-      {
-        color: "#813033",
-      },
-      {
-        lightness: 38,
-      },
-      {
-        visibility: "off",
-      },
-    ],
-  },
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {
-    elementType: "labels",
   },
   {
     featureType: "poi.sports_complex",
-    stylers: [
-      {
-        color: "#9e5916",
-      },
-      {
-        lightness: 32,
-      },
-    ],
-  },
-  {},
-  {
-    featureType: "poi.government",
-    stylers: [
-      {
-        color: "#9e5916",
-      },
-      {
-        lightness: 46,
-      },
-    ],
-  },
-  {
-    featureType: "transit.station",
+    elementType: "all",
     stylers: [
       {
         visibility: "off",
@@ -165,37 +54,18 @@ export default [
     ],
   },
   {
-    featureType: "transit.line",
+    featureType: "transit.station.bus",
+    elementType: "all",
     stylers: [
       {
-        color: "#813033",
+        visibility: "on",
       },
       {
-        lightness: 22,
+        saturation: "21",
+      },
+      {
+        weight: "4.05",
       },
     ],
   },
-  {
-    featureType: "transit",
-    stylers: [
-      {
-        lightness: 38,
-      },
-    ],
-  },
-  {
-    featureType: "road.local",
-    elementType: "geometry.stroke",
-    stylers: [
-      {
-        color: "#f19f53",
-      },
-      {
-        lightness: -10,
-      },
-    ],
-  },
-  {},
-  {},
-  {},
 ];
