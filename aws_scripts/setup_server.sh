a4c9862ba03fef95fb6c1c4487f40c9509b31f48
linus-sh <60885375+linus-sh@users.noreply.github.com>
2020-07-07 11:44:50 +0900

    fix appspec settings

--- aws_scripts/setup_server.sh
+++ aws_scripts/setup_server.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 source /home/ec2-user/.bash_profile
+
+cd /home/ec2-user/
+sudo chown ec2-user:ec2-user rakutabi
 cd /home/ec2-user/rakutabi/
-pwd
 # install paclages
 npm install -g yarn
 yarn 
