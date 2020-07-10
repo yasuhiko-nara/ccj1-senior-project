be9e922a78bee8c4fd4ec8e9dfe7807952a7e260
linus-sh <60885375+linus-sh@users.noreply.github.com>
2020-07-10 17:09:34 +0900

    fix setup server

--- aws_scripts/setup_server.sh
+++ aws_scripts/setup_server.sh
@@ -8,6 +8,18 @@ source /home/ec2-user/.bash_profile
 cd /home/ec2-user/
 sudo chown -R ec2-user:ec2-user rakutabi
 
+# json操作用にjqインストール
+sudo yum install -y jq
+
+#parameter storeからparams 取得して変数envsに入れる　jsonを加工
+env=$(aws ssm get-parameters-by-path --path "/RakuTabi/" | jq '.Parameters | . [0] | .Value')
+
+#編集対象定義
+envfile=/home/ec2-user/rakutabi/.env
+
+# 書き込み
+echo "REACT_APP_googleMapsApiKey=${env}" >> ${envfile}
+
 # move to app directory
 cd /home/ec2-user/rakutabi/
 
