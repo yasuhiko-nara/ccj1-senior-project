9300173b9ec9f890660bd1fb01a226d0f6599c86
Yasuhiko Nara <narayasuhiko3@gmail.com>
2020-07-13 17:02:35 +0900

    added GoogleMap component only for route view

--- components/GoogleMap/RouteSaveButton.js
+++ components/GoogleMap/RouteSaveButton.js
@@ -4,21 +4,23 @@ import { useSelector } from "react-redux";
 import Button from "@material-ui/core/Button";
 
 export default function () {
-  const userLoginFlag = useSelector((state) => state.users.longinFlag);
+  const userLoginFlag = useSelector((state) => state.users.loginFlag);
   const userId = useSelector((state) => state.users.userId);
-  //   const userName = useSelector((state) => state.users.userName);
 
   const saveRoute = useCallback(async () => {
-    const res = await axios({
+    const data = JSON.stringify(["data1", "data2", "data3"]);
+    const opt = {
       method: "post",
+      headers: {
+        Authorization: process.env.Authorization,
+      },
       url:
         "https://e2uo59wqde.execute-api.ap-northeast-1.amazonaws.com/savedRoot",
-      data: {
-        userId: userId,
-        root: [],
-      },
-    });
-  });
+      data: data,
+    };
+    const res = await axios(opt);
+    console.log("saved route data and this is the response", res.data);
+  }, []);
 
   //   export async function getStaticProps() {
   //     const res = await axios.get(
