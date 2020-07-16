3f60b6220dd0ef6e4b25ae464800aec25033ae86
Urara <Urara.Nakauchi@gmail.com>
2020-07-16 14:48:34 +0900

    Improved appearance of SheduleOfADay

--- components/ScheduleOfADay.js
+++ components/ScheduleOfADay.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { makeStyles } from "@material-ui/core/styles";
 import Accordion from "@material-ui/core/Accordion";
 import AccordionSummary from "@material-ui/core/AccordionSummary";
@@ -12,14 +13,23 @@ import RouteSaveButton from "./GoogleMap/RouteSaveButton";
 const useStyles = makeStyles((theme) => ({
   root: {
     width: "100%",
+    // alignItems: "center",
   },
   heading: {
     fontSize: theme.typography.pxToRem(15),
     fontWeight: theme.typography.fontWeightRegular,
+    flexBasis: "25%",
+    alignSelf: "center",
   },
+  button: { flexBasis: "20%" },
+  margin: { flexBasis: "35%" },
 }));
 
 export default function ScheduleOfADay({ schedules, routeInfo }) {
+  const userLoginFlag = useSelector((state) => state.users.loginFlag);
+  const currentDirection = useSelector(
+    (state) => state.travels.currentDirection
+  );
   const classes = useStyles();
   const distanceInKm =
     routeInfo.length > 0
@@ -37,31 +47,41 @@ export default function ScheduleOfADay({ schedules, routeInfo }) {
       : null;
 
   const durationTime = durationInSec
-    ? new Date(durationInSec * 1000).toISOString().substr(11, 8)
+    ? new Date(durationInSec * 1000).toISOString().substr(11, 5)
     : null;
 
   return (
     <>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography className={classes.heading}>
-            移動距離：{distanceInKm}km
-          </Typography>
+      {userLoginFlag && currentDirection && (
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Typography className={classes.heading} item={4}>
+              移動距離： {distanceInKm}km
+            </Typography>
 
-          <Typography className={classes.heading}>
-            所要時間：{durationTime}
-          </Typography>
-          <RouteDeleteButton />
-          <RouteSaveButton />
-        </AccordionSummary>
-        <AccordionDetails>
-          <Breadcrumb schedules={schedules} />
-        </AccordionDetails>
-      </Accordion>
+            <Typography className={classes.heading} item={4}>
+              所要時間： {durationTime}
+            </Typography>
+
+            <div className={classes.margin} />
+
+            <div className={classes.button}>
+              <RouteDeleteButton />
+            </div>
+
+            <div className={classes.button}>
+              <RouteSaveButton />
+            </div>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Breadcrumb schedules={schedules} />
+          </AccordionDetails>
+        </Accordion>
+      )}
     </>
   );
 }
