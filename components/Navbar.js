2acba82fa9fd2de64eeb6cab84ca51391307fcd6
ArakakiTakuya <225arataku@gmail.com>
2020-07-19 23:22:44 +0900

    fixed the problem

--- components/Navbar.js
+++ components/Navbar.js
@@ -84,14 +84,17 @@ const Navbar = () => {
 
   const handleChange = (event) => {
     dispatch(set_prefecture(event.target.value));
-    Router.push({
-      pathname: "/map",
-      query: {
-        lat: event.target.value.lat,
-        lng: event.target.value.lng,
-        pref: event.target.value.pref,
-      },
-    });
+    //console.log(Router.pathname);
+    if (Router.pathname !== "/recommendation") {
+      Router.push({
+        pathname: "/map",
+        query: {
+          lat: event.target.value.lat,
+          lng: event.target.value.lng,
+          pref: event.target.value.pref,
+        },
+      });
+    }
   };
 
   useEffect(() => {
